b31f42005d5133d994b6068a61b63632dd51baac
Anissa1989 <anissa.delli.b@gmail.com>
2025-07-24 17:08:11 +0200

    exo 1
    Co-Authored-By: Salomikasa <133390425+Salomikasa@users.noreply.github.com>
    Co-Authored-By: Rawand2974 <222383158+Rawand2974@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log(Hello Javascript!);
+console.log("Hello Javascript!")
+
+function getComputerChoice(choices){
+//let choices = ["Pierre","Papier", "Ciseaux"];
+let randomChoices = choices[Math.floor(Math.random() * choices.length)];
+return randomChoices
+}
+let choiceComputer = getComputerChoice(["Pierre","Papier", "Ciseaux"])
+console.log("Votre adversaire joue " + choiceComputer);
 
