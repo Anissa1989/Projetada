87da95052b3673daaf74adb15ac792e55860e133
Salomikasa <133390425+Salomikasa@users.noreply.github.com>
2025-07-25 09:58:06 +0200

    mise en page
    j'ai enlevé un doublon de javascript
    
    Co-Authored-By: Anissa1989 <222383048+Anissa1989@users.noreply.github.com>
    Co-Authored-By: Rawand2974 <222383158+Rawand2974@users.noreply.github.com>

---
+++ test.js
@@ -0,0 +1,12 @@
+function getComputerChoice(chiffre) {
+    if (chiffre === 1) {
+        return "Papier"
+    }
+    if (chiffre === 2) {
+        return "Caillou"
+    }
+    if (chiffre === 3) {
+        return "Ciseaux"
+    }  {return Math.floor(Math.random() * max);
+}}
+console.log(MgetComputerChoice());
