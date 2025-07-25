1fa6c8ad7905b02a66e2ae3bef06bdc31d817937
Rawand2974 <rawand2974@gmail.com>
2025-07-25 10:52:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log("Hello Javascript!")
-=======
-console.log("Hello Javascript");
->>>>>>> Stashed changes
+
 
 function getComputerChoice(choices){
 //let choices = ["Pierre","Papier", "Ciseaux"];
